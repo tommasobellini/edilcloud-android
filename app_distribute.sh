8644a2c3947db23804c44f3a10ca0e29153700d4
tommasobellini <dev@tommasobellini.it>
2021-02-09 23:37:50 +0100

    # last changes

--- app_distribute.sh
+++ app_distribute.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #firebase appdistribution:distribute build/app/outputs/apk/release/app-release.apk --app 1:1081576602759:android:b9fff8e8b1c67fe6d8cc22 --groups "admin" --release-notes "Android pure app"  # --testers "bellinitom97@gmail.com"
-firebase appdistribution:distribute app/build/outputs/apk/debug/app-debug.apk --app 1:1081576602759:android:b9fff8e8b1c67fe6d8cc22 --groups "admin" --release-notes "fixed camera photo orientation"  # --testers "bellinitom97@gmail.com"
+firebase appdistribution:distribute app/build/outputs/apk/debug/app-debug.apk --app 1:1081576602759:android:b9fff8e8b1c67fe6d8cc22 --groups "admin" --release-notes "fixed plans page"  # --testers "bellinitom97@gmail.com"
